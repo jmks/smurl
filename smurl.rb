1e19f8877758e5165465ac5e91dc5ffc08df4722
jmks <jason.schweier@gmail.com>
2014-06-15 12:17:22 -0400

    added layout and mock smurl post

--- smurl.rb
+++ smurl.rb
@@ -7,4 +7,22 @@ class Smurl < Sinatra::Base
     haml :index  
   end
 
+  post '/' do 
+    @url           = params[:url]
+    @shortened_url = get_or_set_short_url(@url)
+
+    haml :index
+  end
+
+  helpers do
+    def get_or_set_short_url url
+      "smurl"
+    end
+
+    def shrinkage(original, short)
+      percent = 100.0 * short.length / original.length
+      "#{percent}%"
+    end
+  end
+
 end
